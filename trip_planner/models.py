67454322b1f7c1c64fa865180690dddf5af0302c
Fire-Fairy84 <esther.sarasua@gmail.com>
2024-08-24 17:07:51 +0200

    initial commit

--- trip_planner/models.py
+++ trip_planner/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class User(models.Model):
@@ -7,5 +8,6 @@ class User(models.Model):
     password = models.CharField(max_length=20)
     email = models.EmailField(max_length=200)
 
+
 def __str__(self):
-	return f'User: {self.name}'
+    return f'User: {self.name}'
--- trip_planner/serializer.py
+++ trip_planner/serializer.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from trip_planner.models import User
 
+
 # Serializamos la data que trae ese objeto, en este caso el objeto Person
 class UserSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = User
-		# Le decimos cuáles son los campos de mi modelo que quiero que me estructure. Serialización de la data. Aquí le decimos que queremos que serialice todos los campos
-		fields = '__all__'
+    class Meta:
+        model = User
+        # Le decimos cuáles son los campos de mi modelo que quiero que me estructure. Serialización de la data. Aquí
+        # le decimos que queremos que serialice todos los campos
+        fields = '__all__'
