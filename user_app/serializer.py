6996e3a1996b43d7f43b56a44286574c0f90c86d
Fire-Fairy84 <esther.sarasua@gmail.com>
2024-08-29 23:15:01 +0200

    create connection to front

--- user_app/serializer.py
+++ user_app/serializer.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 # Serializamos la data que trae ese objeto, en este caso el objeto User
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
         # Le decimos cuáles son los campos de mi modelo que quiero que me estructure. Serialización de la data. Aquí
@@ -18,4 +19,3 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data['password'],
         )
         return user
-
--- user_app/views.py
+++ user_app/views.py
@@ -4,6 +4,7 @@ from .serializer import RegisterSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
+
 # Create your views here.
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -21,4 +22,3 @@ class LogoutView(views.APIView):
             return Response({"message": "Token deleted"}, status=status.HTTP_200_OK)
         except Token.DoesNotExist:
             return Response({"message": "Token not found"}, status=status.HTTP_400_BAD_REQUEST)
-
